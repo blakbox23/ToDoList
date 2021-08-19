ae8b898f4ffbb0ba61e210f9f22f744aaa4c4c96
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-08-19 09:09:25 +0300

    Persist task status through local storage

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import { indexOf } from 'lodash';
 import './style.css';
-const tasks = [
-  {
-    description: 'Complete list structure',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Complete interactive list task',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Work on adding and removing tasks',
-    completed: false,
-    index: 3,
-  },
-];
+// const tasks = [
+//   {
+//     description: 'Complete list structure',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     description: 'Complete interactive list task',
+//     completed: false,
+//     index: 2,
+//   },
+//   {
+//     description: 'Work on adding and removing tasks',
+//     completed: false,
+//     index: 3,
+//   },
+// ];
+
+ 
+ const tasks = getFromLocalStorage();
 
 const completedTasks = [];
 
@@ -38,8 +41,7 @@ function ui() {
     checkbox.value = `${tasks[i]}`;
     checkbox.classList.add('chkbx');
 
-
-    pTask.innerText = `${tasks[i].description}`;
+ 
 
     li.appendChild(lDiv);
 
@@ -52,14 +54,25 @@ function ui() {
 
     ul.appendChild(li);
 
+    if(tasks[i].completed == true){
+      pTask.innerText = `${tasks[i].description}`;
+      pTask.classList.add('cancel');
+      console.log( pTask.previousSibling.children[0])
+      pTask.previousSibling.children[0].checked = true;
+      
+    }else{
+      pTask.innerText = `${tasks[i].description}`;
+    }
+
     
     checkbox.addEventListener('change', (e) => {
       if (e.target.checked) {
         taskCompleted(tasks[i]);
-        check();
-
+        e.target.parentElement.nextSibling.classList.add('cancel');
       } else {
         taskNotComplete(tasks[i]);
+        e.target.parentElement.nextSibling.classList.remove('cancel');
+
 
       }
     });
@@ -73,6 +86,7 @@ function taskCompleted(task){
   completedTasks.push(task);
   // console.log(completedTasks)
   // console.log(tasks);
+  addToLocalStorage()
 
 }
 
@@ -82,9 +96,31 @@ function taskNotComplete(task){
   completedTasks.splice(i, 1);
   // console.log(completedTasks)
   // console.log(tasks);
+  addToLocalStorage()
 }
 
+function addToLocalStorage(){
+const storage = JSON.stringify(tasks);
+localStorage.setItem('lstore', storage);
+}
+
+function getFromLocalStorage(){
+  const storage = JSON.parse(localStorage.getItem('lstore')) || tasks;
+  return storage;
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const tasks = getFromLocalStorage();
+//   for(let i=0; i<fromStorage.length; i++){
+//     if(fromStorage[i].completed == true){
+      
+//     }
+//   }
+// })
+
 ui();
+
+
 
 
 
